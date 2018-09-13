e3f4e9cc5af731d1d2526928cbf2f66026e78c21
Tiago Marques <tiagomarquesdeveloper@gmail.com>
2018-09-13 14:37:55 +0100

    Consola para teste API

---
+++ FleetManagement/ConsoleRestTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRestTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeEventAsync objsync = new ConsumeEventAsync();
+            objsync.GetAllFuelData();
+        }
+    }
+}
