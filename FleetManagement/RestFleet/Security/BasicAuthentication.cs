9f794c338d30e0d196d4ebc5218d1e15e846d411
Baptista <Bruno_baptista86@hotmail.com>
2018-09-19 16:43:10 +0100

    basicAuthentication

---
+++ FleetManagement/RestFleet/Security/BasicAuthentication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace RestFleet.Security
+{
+    public class BasicAuthentication : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if(actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                string decodeAuthToken = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                string[] usernamePasswordArray = decodeAuthToken.Split(':');
+
+                string username = usernamePasswordArray[0];
+                string password = usernamePasswordArray[1];
+                
+                //if valida user
+                //
+                //else actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+
+
+            }
+        }
+    }
+}
